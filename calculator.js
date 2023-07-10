e11fe6a0908dbc6f1cf2c8f448a2e1bdfce016b7
sid56rajawat <sid56rajawat@gmail.com>
2023-07-10 19:02:30 +0530

    Add basic calc functionality with display
    allows you to enter expressions continuously and get output.
    
    each touch generates a response in display.
    
    when the operator are presed it evaluates previous expression.
    
    might be some erros and unhandled edge cases like evaluating negative vals.

--- calculator.js
+++ calculator.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const OPRERATORS = ["+","-","*","/"];
 const add = (a,b) => a+b;
 const subtract = (a,b) => a-b;
 const multiply = (a,b) => a*b;
@@ -19,4 +20,54 @@ let calculator = {
     }
 };
 
-module.exports = {...calculator};
+let display = document.querySelector(".display");
+// changing color of buttons when pressed
+let tokenButtons = [...document.querySelectorAll(".token")];
+tokenButtons.forEach(tokenButton => {
+    tokenButton.addEventListener('click', (e) => {
+        e.target.classList.add('pressed');
+    });
+});
+
+function removeTransition(e){
+    if(e.propertyName !== "background-color") return;
+    e.target.classList.remove('pressed');
+};
+tokenButtons.forEach(tokenButton => {
+    tokenButton.addEventListener('transitionend', removeTransition);
+});
+// Create the functions that populate the 
+// display when you click the number buttons.
+let numberButtons = tokenButtons.filter((token) =>{
+    let val = token.textContent;
+    return (val >= '0' && val <= '9');
+});
+numberButtons.forEach((numberButton) => numberButton.addEventListener('click',(e) => {
+    display.innerText += ` ${numberButton.innerText}`;
+}));
+
+// when operators are pressed concat them to display
+function evaluatePrevious(){
+    let expr = display.textContent;
+    console.log(expr);
+    let split = expr.split(" ");
+    console.log(split);
+    if(split.length === 3){
+        display.innerText = calculator.operate(expr);
+    }
+}
+
+function concatToDisplay(e){
+    display.innerText += ` ${e.target.textContent}`;
+}
+let operatorButtons = tokenButtons.filter((token) => OPRERATORS.includes(token.innerText));
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener('click', (e) => {
+        evaluatePrevious();
+        concatToDisplay(e);
+    });
+});
+
+// ToDo: equal and CE
+
+// module.exports = {...calculator};
